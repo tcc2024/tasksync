6012128a91847142beaac6c2d9fe377e45133dfd
tcc2024 <etec.tccds2024@gmail.com>
2024-03-20 22:27:00 -0300

    PrimeiroPasso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './pages/Login.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+        <div className='left'>
+          <h1>Receba</h1>
+        </div>
+          <h1>Login</h1>
     </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='left'>
+        <div className='background'>
+            <div className='container'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
