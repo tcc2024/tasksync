36880b781e73aad90ff066ca2060414330ad5e3b
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-03-27 22:26:53 -0300

    Implemetando o Front

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import Login from './pages/Login';
+import "@fontsource/inter"; 
+
 
 function App() {
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,56 +1,35 @@
-import React, { useState } from 'react';
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import "@fontsource/inter";
+import icone from "../assets/Email.png";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aqui você pode adicionar a lógica para enviar as credenciais para o servidor e autenticar o usuário
-  };
-  
   return (
-    <div className='background'>
-      <div className='left'>
-          <h1>LOG-IN</h1>
-          <p>Bem Vindo de volta!</p>
-        </div>
-      <div className='right'>
-            <div className='container'>
-            <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor='username'>Username: </label>
-              <input
-                type='text'
-                id='username'
-                value={username}
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <div>
-              <label htmlFor='password'>Password: </label>
-              <input
-                type='password'
-                id='password'
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <button type='submit'>Login</button>
-          </form>
-
+    <div className="background">
+      <div className="left">
+        <h1 id="login">Sign Up</h1>
+      </div>
+      <div className="right">
+        <div className="input-container">
+          <div className="icon-container">
+            <img src={icone} alt="Email Icon" className="icon" />
           </div>
+          <input
+            id="input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="input-field"
+          />
         </div>
+        <br />
+        <br />
+        <br />
+        <input 
+        id="input"
+         type="password" 
+         name="senha" placeholder="Senha" />
+      </div>
     </div>
-  )
+  );
 }
