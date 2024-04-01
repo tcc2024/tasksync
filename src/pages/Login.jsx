2be2dd2ea375f71baf0dfdf80d7b8149539a806a
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-03-31 21:15:36 -0300

    Arrumando os inputs e centralizando o tempo da div right

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,43 @@
 import React from "react";
 import "./Login.css";
 import "@fontsource/inter";
+import "@fontsource/inter/400.css";
 import icone from "../assets/Email.png";
 
 export default function Login() {
   return (
     <div className="background">
       <div className="left">
-        <h1 id="login">Sign Up</h1>
+        <h1 id="login">Sign-Up</h1>
+        <h3 id="bemvindo">Seja Bem Vindo</h3>
       </div>
       <div className="right">
-        <div className="input-container">
-          <div className="icon-container">
-            <img src={icone} alt="Email Icon" className="icon" />
-          </div>
-          <input
-            id="input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="input-field"
-          />
-        </div>
-        <br />
-        <br />
-        <br />
-        <input 
-        id="input"
-         type="password" 
-         name="senha" placeholder="Senha" />
+        <h1 id="title">Criar Conta</h1>
+        <input
+          id="input-usuario"
+          type="email"
+          name="email"
+          className="inputs"
+          placeholder="Usuario"
+        />
+      </div>
+      <div>
+        <input
+          id="input-email"
+          type="text"
+          name="usuario"
+          className="inputs"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <input
+          id="input-senha"
+          type="password"
+          name="email"
+          placeholder="Senha"
+          className="input-field"
+        />
       </div>
     </div>
   );
