8cfe0805557d9c97f2db17e8094d52e9798e6be0
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-04-03 19:24:47 -0300

    Tela alternativa

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,41 +2,49 @@ import React from "react";
 import "./Login.css";
 import "@fontsource/inter";
 import "@fontsource/inter/400.css";
-import icone from "../assets/Email.png";
+import logo from "../assets/Logo.png";
+import texto from "../assets/Task.Sync.png";
 
 export default function Login() {
   return (
     <div className="background">
-      <div className="left">
-        <h1 id="login">Sign-Up</h1>
-      </div>
-      <div className="right">
-        <h1 id="title">Criar Conta</h1>
-        <input
-          id="input-usuario"
-          type="email"
-          name="email"
-          className="inputs"
-          placeholder="Usuario"
-        />
-      </div>
-      <div>
-        <input
-          id="input-email"
-          type="text"
-          name="usuario"
-          className="inputs"
-          placeholder="Email"
-        />
-      </div>
-      <div>
-        <input
-          id="input-senha"
-          type="password"
-          name="email"
-          placeholder="Senha"
-          className="input-field"
-        />
+      <div className="center">
+        <div>
+          <h1 id="title">Criar Conta</h1>
+        </div>
+        <div>
+          <input
+            id="input-usuario"
+            type="email"
+            name="email"
+            className="inputs"
+            placeholder="Usuario"
+          />
+        </div>
+        <div>
+          <input
+            id="input-email"
+            type="text"
+            name="usuario"
+            className="inputs"
+            placeholder="Email"
+          />
+        </div>
+        <div>
+          <input
+            id="input-senha"
+            type="password"
+            name="email"
+            className="inputs"
+            placeholder="Senha"
+          />
+          <div>
+            <h5>Já Tem Uma Conta? Entre!</h5>
+          </div>
+          <div>
+            <button className="btn">Cadastrar</button>
+          </div>
+        </div>
       </div>
     </div>
   );
