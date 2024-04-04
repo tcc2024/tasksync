37bfa113ea669ff4243e95b961aed475d8106fa6
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-04-04 19:21:01 -0300

    TelaHome e Buttons ( react-dom-router )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import Login from './pages/Login';
-import "@fontsource/inter"; 
 
 
 function App() {
-  return (
-    <Login/>
+  return (  
+    <Home></Home>
+    
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="top">
+        <button className="cadastrar">Cadastrar</button>
+        <button className="entrar">Entrar</button>
+    </div>
+  );
+}
