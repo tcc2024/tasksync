ba896c95175b2b685ebfdcc3316b004481cfd26d
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-05-14 22:07:39 -0300

    Criando MenuProjeto

--- src/pages/MenuProjeto/MenuProjeto.jsx
+++ src/pages/MenuProjeto/MenuProjeto.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import HeaderProjeto from '../../Componets/Header/HeaderProjeto'
+import React from "react";
+import HeaderProjeto from "../../Componets/Header/HeaderProjeto";
 import Sidebar from "../../Componets/Sidebar/Sidebar";
 import { ToastContainer } from "react-toastify";
-import styles from "./MenuProjeto.module.css"
+import styles from "./MenuProjeto.module.css";
 
 export default function MenuProjeto() {
   return (
     <div className={styles.container}>
-    <Sidebar />
-    <div className={styles.appContainer}>
-      <HeaderProjeto />
-      <div className={styles.pages}>
-        <div className={styles.afazer}>
-
-        </div>
-        <div classname={styles.emandamento}>
-
-        </div>
-        <div classname={styles.concluida}>
-
+      <Sidebar />
+      <div className={styles.appContainer}>
+        <HeaderProjeto />
+        <div className={styles.pages}>
+          <div className={styles.column}>
+            <h2>
+              <center>A Fazer</center>
+            </h2>
+            <div className={styles.task}>Tarefa 1</div>
+            <div className={styles.task}>Tarefa 2</div>
+            <center>
+              <button className={styles.botao}>Adicionar Tarefa</button>
+            </center>
+          </div>
+          <div className={styles.column}>
+            <h2>
+              <center>Em Andamento</center>
+            </h2>
+            <div className={styles.task}>Tarefa 3</div>
+            <div className={styles.task}>Tarefa 4</div>
+            <center>
+              <button className={styles.botao}>Adicionar Tarefa</button>
+            </center>
+          </div>
+          <div className={styles.column}>
+            <h2>
+              <center>Concluída</center>
+            </h2>
+            <div className={styles.task}>Tarefa 5</div>
+            <div className={styles.task}>Tarefa 6</div>
+            <center>
+              <button className={styles.botao}>Adicionar Tarefa</button>
+            </center>
+          </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
-    <ToastContainer />
-  </div>
-  )
+  );
 }
